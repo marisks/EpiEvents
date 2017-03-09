cf8f48e06f90a713704e27f70334298ff0de9b4a
Māris Krivtežs <marisks@gmail.com>
2017-03-09 23:43:08 +0200

    Added assembly info.

--- src/EpiEvents.Core/Properties/AssemblyInfo.cs
+++ src/EpiEvents.Core/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EpiEvents.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An in-process messaging style event handling for Episerver")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Maris Krivtezs")]
 [assembly: AssemblyProduct("EpiEvents.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Maris Krivtezs 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
