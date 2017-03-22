29b82bd0b059bbc481968d389a8c3fd6a2a178ae
Māris Krivtežs <marisks@gmail.com>
2017-03-22 19:34:32 +0200

    Added sample handler.

---
+++ src/EpiEvents.Core.Tests/SampleHandler.cs
@@ -0,0 +1,13 @@
+﻿using EpiEvents.Core.Events;
+using MediatR;
+
+namespace EpiEvents.Core.Tests
+{
+    public class SampleHandler : INotificationHandler<CreatedContent>
+    {
+        public void Handle(CreatedContent notification)
+        {
+            // Handle your event
+        }
+    }
+}
