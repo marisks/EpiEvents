06ec710a31bbf9ca65e098ee83e31688b3342339
Māris Krivtežs <marisks@gmail.com>
2017-03-22 21:34:32 +0200

    Added mediator initialization.

---
+++ src/EpiEvents.Core/Initialization.cs
@@ -0,0 +1,20 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace EpiEvents.Core
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class Initialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            var mediator = context.Locate.Advanced.GetInstance<EventsMediator>();
+            mediator.Initialize();
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
